54d71291e363762a2ddac7e964eb786a9687e637
Nikita <nikitsidenk@mail.ru>
2019-10-08 17:10:48 +0300

    edit config / git ignore

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,18 @@
 module.exports = {
   plugins: [
-    // css mixins can be installed
-    // bem postcss can be installed
-    // install destyle.css
-    require('postcss-flexbugs-fixes'),
-    require('cssnano')({
-      preset: 'default',
+    require("postcss-flexbugs-fixes"),
+    require("cssnano")({
+      preset: "default"
     }),
-    require('postcss-import'),
-    require('postcss-preset-env')({
+    require("postcss-import"),
+    require("postcss-preset-env")({
       stage: 0,
-      autoprefixer: { grid: true },
+      autoprefixer,
       features: {
-        'custom-media-queries': true,
-        'nesting-rules': true,
-      },
-      importFrom: './src/base.css',
-    }),
-  ],
+        "custom-media-queries": true,
+        "nesting-rules": true
+      }
+      // importFrom: './src/base.css',
+    })
+  ]
 };
