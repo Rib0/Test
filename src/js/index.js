40fc5646cd8237a97f95c0f19a96569df328d1f6
Nikita <nikitsidenk@mail.ru>
2019-10-08 17:41:37 +0300

    init project

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import reducer from "store/reducers";
+import App from "./App";
+
+const store = createStore(reducer, applyMiddleware(logger, thunk));
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const SvgStore = require('webpack-svgstore-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const SvgStore = require("webpack-svgstore-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const config = {
-  entry: ['./src/js'],
+  entry: ["./src/js"],
   output: {
-    path: path.resolve(__dirname, 'build/'),
-    filename: !isProd ? '[name].js' : '[name].[hash].js',
+    path: path.resolve(__dirname, "build/"),
+    filename: !isProd ? "[name].js" : "[name].[hash].js"
   },
-  devtool: isProd && 'cheap-module-source-map',
+  devtool: isProd && "cheap-module-source-map",
   devServer: {
     compress: true,
     overlay: true,
@@ -23,7 +22,7 @@ const config = {
     port: 8080,
     hot: true,
     open: true,
-    contentBase: path.resolve(__dirname, 'build/'),
+    contentBase: path.resolve(__dirname, "build/")
   },
   optimization: {
     minimizer: [
@@ -32,48 +31,49 @@ const config = {
         parallel: true,
         uglifyOptions: {
           output: {
-            comments: false,
+            comments: false
           }
-        },
-      }),
-    ],
+        }
+      })
+    ]
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
         test: /\.(css)$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
-              importLoaders: 1,
-            },
+              localIdentName: "[path][name]__[local]--[hash:base64:5]",
+              importLoaders: 1
+            }
           },
-          'postcss-loader',
-        ],
+          "postcss-loader"
+        ]
       },
       {
-          loader: 'file-loader',
-          exclude: /\.(js|css|html|svg)/,
-          options: { 
-              name: 'images/[name].[ext]' 
-          },
-      },
-    ],
+        loader: "file-loader",
+        exclude: /\.(js|css|html|svg)/,
+        options: {
+          name: "images/[name].[ext]"
+        }
+      }
+    ]
   },
   resolve: {
-    extensions: ['.js'],
-    modules: ['node_modules', path.resolve(__dirname, 'src/js')],
+    extensions: [".js"],
+    modules: ["node_modules", path.resolve(__dirname, "src/js")],
     alias: {
-      images: path.resolve(__dirname, 'src/images/')
+      images: path.resolve(__dirname, "src/images/"),
+      svg: path.resolve(__dirname, "src/svg/")
     }
   },
   stats: {
@@ -85,32 +85,31 @@ const config = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: !isProd ? '[name].css' : '[name].[hash].css',
+      filename: !isProd ? "[name].css" : "[name].[hash].css"
     }),
     new HtmlWebpackPlugin({
       inject: false, // inject script at the bottom of the body
       // hash: true, // add hash to files for hash busting
-      template: './index.html', // entry template
-      filename: 'index.html', // output template
+      template: "./index.html", // entry template
+      filename: "index.html" // output template
     }),
-    isProd && new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false,
-    }),
+    isProd &&
+      new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false
+      }),
     new CopyPlugin([
-      { 
-        from: 'src/images', 
-        to: 'images',
-        force: true 
-      },
+      {
+        from: "src/images",
+        to: "images",
+        force: true
+      }
     ]),
     new SvgStore({
       svgoOptions: {
-        plugins: [
-          { removeTitle: true }
-        ]
-      },
+        plugins: [{ removeTitle: true }]
+      }
     })
-  ].filter(Boolean),
+  ].filter(Boolean)
 };
 
 module.exports = config;
