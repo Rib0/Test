d46f7d4dab50848498f62885a4737248c21d4aa4
Nikita <nikitsidenk@mail.ru>
2019-10-21 22:05:53 +0300

    some done

--- src/js/components/Main/index.js
+++ src/js/components/Main/index.js
@@ -4,32 +4,52 @@ import Button from 'components/Button';
 import styles from './styles.css';
 
 import { items } from 'components/data/list.json';
+import Sidebar from 'components/Sidebar';
+import Container from 'components/Container';
+
+import close from 'Images/close.png';
 
 export default class Main extends Component {
     render() {
         return (
-            <div className={styles.main}>
-                <div className={styles.header}>
-                    <input type="text" className={styles.input} />
-                </div>
-                <div className={styles.preview}>
-                    <ul className={styles.list}>
-                        <li className={styles.list__header}>
-                            <Button className={styles.button} text="Создать заявку" />
-                            <div className={styles.names}>
-                                <span className={styles.id}>ID</span>
-                                <span className={styles.name}>Название</span>
-                            </div>
-                        </li>
-                        {items.map(item => (
-                            <li className={styles.list__item} key={item.id}>
-                                <span className={styles.id}>{item.id}</span>
-                                <span className={styles.name}>{item.name}</span>
+            <Container>
+                <Sidebar />
+                <div className={styles.main}>
+                    <div className={styles.header}>
+                        <input type="text" className={styles.input} />
+                    </div>
+                    <div className={styles.preview}>
+                        <ul className={styles.list}>
+                            <li className={styles.list__header}>
+                                <Button className={styles.button} text="Создать заявку" />
+                                <div className={styles.names}>
+                                    <span className={styles.id}>ID</span>
+                                    <span className={styles.name}>Название</span>
+                                </div>
                             </li>
-                        ))}
-                    </ul>
+                            {items.map(item => (
+                                <li className={styles.list__item} key={item.id}>
+                                    <span className={styles.id}>{item.id}</span>
+                                    <span className={styles.name}>{item.name}</span>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className={styles.content}>
+                            <div className={styles.content__header}>
+                                <span className={styles.content__number}>№ 67405</span>
+                                <p className={styles.content__text}>
+                                    Просьба оценить разработку рекламного баннера на новорижском
+                                    шоссе. Форматы 500x500x30. Материал - полиестирол хорошего
+                                    качества.
+                                </p>
+                                <img className={styles.close} src={close} alt="close" />
+                            </div>
+                            <div className={styles.content__main}>qweqe</div>
+                            <div className={styles.content__status}>qweqw</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </Container>
         );
     }
 }
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 
-import Container from 'components/Container';
 import Main from 'components/Main';
-import Sidebar from 'components/Sidebar';
 
 export default class App extends Component {
     render() {
-        return (
-            <Container>
-                <Sidebar />
-                <Main />
-            </Container>
-        );
+        return <Main />;
     }
 }
